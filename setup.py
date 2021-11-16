e051d606665b36a96431c4c959525b46f19e7847
Richard Lane <richard.lane@bristol.ac.uk>
2021-11-16 11:01:11 +0000

    add a pypi long description

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
     name="fourbody",
     packages=["fourbody"],
-    version="0.1",
+    version="0.2",
     license="MIT",
     description="Phase space parameterisation for four-body decays X -> h1+ h2- h3- h4+",
+    long_description="See the projet homepage for details",
     author="Richard Lane",
     author_email="richard.lane6798@gmail.com",
     url="https://github.com/richard-lane/fourbody",
-    download_url="https://github.com/richard-lane/fourbody/archive/v_01.tar.gz",
+    download_url="https://github.com/richard-lane/fourbody/archive/v_02.tar.gz",
     keywords=[
         "Physics",
         "hadron",
